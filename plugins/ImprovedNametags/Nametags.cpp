c1f53ea17214debef659871832ed728e3a15e615
tidehunter1 <76674623+tidehunter1@users.noreply.github.com>
2026-08-21 21:07:56 +0800

    Enhance address resolution in Nametags.cpp

--- plugins/ImprovedNametags/Nametags.cpp
+++ plugins/ImprovedNametags/Nametags.cpp
@@ -449,10 +449,15 @@ private:
 		static uintptr_t address = 0;
 		if (!tried_resolve) {
 			tried_resolve = true;
-			address = GW::Scanner::Find(
-				"\x55\x8b\xec\x8b\x4d\x08\x3b\x0d\x00\x00\x00\x00\x72\x04\x33\xc0\x5d\xc3\xa1\x00\x00\x00\x00\x8b\x04\x88\x5d\xc3",
-				"xxxxxxxx????xxxxxxx????xxxxx"
+			const uintptr_t call_site = GW::Scanner::FindAssertion(
+				"avselect.cpp",
+				"!(manualAgentId && !ManagerFindAgent(manualAgentId))",
+				0x1a3,
+				-0x16
 			);
+			if (call_site) {
+				address = GW::Scanner::FunctionFromNearCall(call_site);
+			}
 		}
 		return address;
 	}
