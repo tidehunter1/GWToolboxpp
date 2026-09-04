6d64a21cfd991f0a612e878972c9522f1915022a
tidehunter1 <76674623+tidehunter1@users.noreply.github.com>
2026-09-04 20:38:48 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- plugins/ImprovedNametags/Nametags.cpp
+++ plugins/ImprovedNametags/Nametags.cpp
@@ -341,6 +341,7 @@ private:
 	static constexpr float kEmbarkRearmHysteresisPct = 5.f;
 
 	struct AgentState {
+		std::optional<ImU32> last_pushed_color;
 		bool we_applied_flag = false;
 		bool has_quest_marker = false;
 	};
@@ -526,6 +527,29 @@ private:
 		return true;
 	}
 
+	using PoolColorSetter_pt = void(__cdecl*)(uint32_t, uint32_t, const uint32_t*);
+	static inline PoolColorSetter_pt PoolColorSetter_Func = nullptr;
+
+	static bool EnsurePoolColorSetterScanned() {
+		if (PoolColorSetter_Func) return true;
+		if (!ResolveScannedFunc(PoolColorSetter_Func,
+			"\x55\x8b\xec\x53\x56\x57\xff\x35\xa4\xf5\xa5\x00\xff\x75\x08\xe8\x6c\x6a\xe0\xff\x8b\xf8\x83\xc4\x08\x85\xff\x75\x14\x68\xa4\x00\x00\x00\xba\x38\xbf\xa5\x00\xb9\x70\xf6\x93\x00\xe8\xcf\xe7\xe1\xff\x8b\x47\x0c\x83\xf8\x05\x74\x12\x50\x6a\x05\x68\x94\xbf\xa5\x00\x6a\x02\xe8\x38\x59\xe0\xff\x83\xc4\x10\x83\x7f\x0c\x05\x74\x14\x68\xa8\x00\x00\x00\xba\x38\xbf\xa5\x00\xb9\xcc\xbf\xa5\x00\xe8\x9b\xe7\xe1\xff\x8b\x5d\x0c\x3b\x9f\xa4\x00\x00\x00\x72\x14\x68\x39\x07\x00\x00\xba\x3c\xd2",
+			"xxxxxxxx????xxxx????xxxxxxxxxxxxxxx????x????x????xxxxxxxxxxxx????xxx????xxxxxxxxxxxxxxx????x????x????xxxxxxxxxxxxxxxxx??")) return false;
+		return true;
+	}
+
+	[[nodiscard]] static ImU32 GetNativeAllegianceColor(const GW::AgentLiving* living) noexcept {
+		if (AllegianceColor_Ret) {
+			uint32_t out_color = 0;
+			void* ctx = const_cast<void*>(static_cast<const void*>(living));
+			uint32_t* result = AllegianceColor_Ret(ctx, &out_color, 0);
+			if (result) return static_cast<ImU32>(*result);
+		}
+		if (living->allegiance == GW::Constants::Allegiance::Enemy) return static_cast<ImU32>(0xFFFF0000u);
+		if (living->allegiance == GW::Constants::Allegiance::Npc_Minipet) return static_cast<ImU32>(0xFFA0FF00u);
+		return static_cast<ImU32>(0xFF00FF00u);
+	}
+
 	void OnRevealHotkeyStateChanged() {
 		const bool now_active = ctrl_reveal_down_ || alt_reveal_down_;
 		if (now_active == hide_hotkey_active_) return;
@@ -568,6 +592,8 @@ private:
 			if (SetNameTagBit_Func) {
 				SetNameTagBit_Func(agent, GW::NameTagFlags_ManualTarget, want_flag ? 1 : 0);
 			}
+			uint32_t agent_id_copy = agent_id;
+			GW::UI::SendUIMessage(GW::UI::UIMessage::kAgentUpdate, &agent_id_copy, nullptr);
 		});
 	}
 
